5561233f612d2384ed078d7239c29f3e9d3f3b42
MVA27 <manvekar906@gmail.com>
2022-08-09 14:12:22 +0530

    feat: implemented otp service file

---
+++ Android/otp.php
@@ -0,0 +1,97 @@
+<?php
+
+	//otp.php?type=<get|update>&userName=mehulanvekar&password=newpw
+
+	//This code is almost same as verify_user.php, the only difference in this code it that user password is not required
+
+    if(count($_REQUEST) == 0){
+	    http_response_code(400);
+	    exit(0);
+	}   
+	
+	if(isset($_REQUEST['type'])){
+		$type = $_REQUEST['type'];
+	}
+	else {
+		http_response_code(400);
+	    exit(0);
+	}
+
+	$file = fopen("../Configuration-Files/database.config", "r") or die("Unable to open database.config file!");
+    $IP_ADDRESS = chop(fgets($file));
+    $USER_NAME = chop(fgets($file));
+    $PASSWORD = chop(fgets($file));
+    $DATABASE = chop(fgets($file));
+	$connection = mysqli_connect($IP_ADDRESS,$USER_NAME,$PASSWORD,$DATABASE);
+
+	//'get' section requires only user name
+	if($type == 'get'){
+
+		class Users
+		{
+		    public $id;
+		    public $first_name;
+		    public $last_name;
+		    public $user_name;
+		    public $age;
+		    public $phone_number;
+		    public $isroot;
+		}
+
+
+		if(isset($_REQUEST['userName'])){
+
+			$user_name = $_REQUEST['userName'];
+			$query = "SELECT * FROM users where user_name='$user_name'";
+			$result = mysqli_query($connection,$query);
+			if($result){
+				$row = mysqli_fetch_array($result);
+				fetchAndReturnUserDetails($connection,$user_name);
+				http_response_code(200);
+			}
+			else{
+				http_response_code(400);
+			}
+
+		}
+	}
+
+	//'update' section requires user name and password
+	else if($type == 'update'){
+
+		if(isset($_REQUEST['password']) && isset($_REQUEST['userName'])){
+
+			$user_name = $_REQUEST['userName'];
+			$password = $_REQUEST['password'];
+			$password = sha1($password);
+
+			$query = "UPDATE users SET password='$password' WHERE user_name='$user_name'";
+			$result = mysqli_query($connection,$query);
+
+			if($result)	http_response_code(200);
+			else http_response_code(400);
+		}
+	}
+	
+	function fetchAndReturnUserDetails($connection,$user_name){
+		 
+		$query = "SELECT u.id,u.first_name,u.last_name,u.user_name,u.age,u.phone_number,ru.isroot FROM users u LEFT OUTER JOIN root_users ru ON u.id = ru.id WHERE user_name='$user_name'";
+		$result = mysqli_query($connection,$query);
+
+		if($result){
+
+			$row=mysqli_fetch_array($result);
+			$users = new Users();
+			$users->id = $row['id'];
+			$users->first_name = $row['first_name'];
+			$users->last_name = $row['last_name'];
+			$users->user_name = $row['user_name'];
+			$users->age = $row['age'];
+			$users->phone_number = $row['phone_number'];
+			if($row['isroot'] == 1) $users->isroot = true;
+			else $users->isroot = false;
+
+			echo json_encode($users);
+		}
+	}
+?>
